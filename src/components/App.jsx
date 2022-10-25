d52d6478f718152fb6ca1012fca2756d79aa574a
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-10-26 00:44:23 +0300

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import {Profile} from './Profile/Profile';
 import user from '../data/user.json';
+import data from '../data/data.json';
+import { Statistics } from './Statistics/Statistics';
 
 
 
@@ -13,6 +15,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </>
   );
 };
